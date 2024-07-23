923274814490f1d81d7098de9499d52d8985931d
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-07-23 16:04:19 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+
+const App: React.FC = () => {
+  return (
+    <Screen2 />
+  );
+};
+
+export default App;
